3dfa6f17bd7542b9a36c2d0fecb3de15d948e376
Lorena <lorena.giddens@outlook.com>
2018-06-18 14:24:48 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    # given a date and a patient, create a new appointment
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    # iterate through all appointments & find those belonging to this doctor
+    Appointment.all.select { |a| a.doctor == self }
+  end
+
+  def patients
+    # iterate over the doctor's appointments & collect the patient that belongs to each appointment
+    appointments.map(&:patient)
+  end
+end
